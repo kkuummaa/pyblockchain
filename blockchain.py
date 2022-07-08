2ac2a27d332ba63dd0cab2fe6240d21e3b14093c
kkuummaa <45517816+kkuummaa@users.noreply.github.com>
2022-07-08 10:06:37 +0000

    create transactions

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,10 @@
-from itertools import chain
+import hashlib
+import json
 import logging
 import sys
 import time
-from unicodedata import name
 
-from numpy import block
+import utils
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
@@ -13,30 +13,57 @@ class BlockChain(object):
     def __init__(self):
         self.transaction_pool = []
         self.chain = []
-        self.create_block(0, 'init_hash')
+        self.create_block(0, self.hash({}))
 
     def create_block(self, nonce, prrevious_hash):
-        block = {
+        block = utils.sorted_dict_by_key({
             'timestamp': time.time(),
             'transactions': self.transaction_pool,
             'nonce': nonce,
             'previous_hash': prrevious_hash
-        }
+        })
         self.chain.append(block)
         self.transaction_pool = []
         return block
+
+    def hash(self, block):
+        sorted_block = json.dumps(block, sort_keys=True)
+        return hashlib.sha256(sorted_block.encode()).hexdigest()
+
+    def add_transaction(self, sender_blockchain_address,
+                        recepient_blockchain_address, value):
+        transaction = utils.sorted_dict_by_key({
+            'sender_blockchain_address': sender_blockchain_address,
+            'recepient_blockchain_address': recepient_blockchain_address,
+            'value': float(value)})
+        self.transaction_pool.append(transaction)
+        return True
 
 def pprint(chains):
     for i, chain in enumerate(chains):
         print(f'{"="*25} Chain {i} {"="*25}')
         for k, v in chain.items():
-            print(f'{k:15}{v}')
+            if k == 'transactions':
+                print(k)
+                for d in v:
+                    print(f'{"-"*40}')
+                    for kk, vv in d.items():
+                        print(f' {kk:30}{vv}')
+            else:
+                print(f'{k:15}{v}')
     print(f'{"*"*25}')        
 
 if __name__ == '__main__':
     block_chain = BlockChain()
-    pprint(block_chain.chain)        
-    block_chain.create_block(5, 'hash_1')
+    pprint(block_chain.chain)     
+
+    block_chain.add_transaction('A', 'B', 1.0)
+    previous_hush = block_chain.hash(block_chain.chain[-1])
+    block_chain.create_block(5, previous_hush)
     pprint(block_chain.chain)
-    block_chain.create_block(2, 'hash_2')
+    
+    block_chain.add_transaction('C', 'D', 2.0)
+    block_chain.add_transaction('X', 'Y', 2.0)
+    previous_hush = block_chain.hash(block_chain.chain[-1])
+    block_chain.create_block(2, previous_hush)
     pprint(block_chain.chain)
